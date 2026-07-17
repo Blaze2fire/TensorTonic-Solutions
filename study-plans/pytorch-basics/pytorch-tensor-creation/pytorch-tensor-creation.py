2ac1fa247dba7b55c634aecdc87464a9f357af41
Siddharth <siddharthsekhar19@gmail.com>
2026-07-17 21:01:07 +0530

    Solve pytorch-tensor-creation

--- study-plans/pytorch-basics/pytorch-tensor-creation/pytorch-tensor-creation.py
+++ study-plans/pytorch-basics/pytorch-tensor-creation/pytorch-tensor-creation.py
@@ -4,7 +4,10 @@ def create_tensor(method, shape, value=0.0):
     """
     Returns: list
     """
-    if method=='full':
+    if method=='zeros':
+        return torch.zeros(shape).tolist()
+    elif method=='ones':
+        return torch.ones(shape).tolist()
+    else :
         return torch.full(shape,value).tolist()
-    factory=getattr(torch,method,None)
-    return factory(shape).tolist()
+
